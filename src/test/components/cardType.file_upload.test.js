e8f241b4d5aa27f783040291ce156e29377a9809
AlexDalencourt <alex.dalencourt@gmail.com>
2021-05-11 22:35:10 +0200

    test quality on CardType

--- src/test/components/cardType.file_upload.test.js
+++ src/test/components/cardType.file_upload.test.js
@@ -7,63 +7,67 @@ import { when } from 'jest-when';
 jest.mock('axios');
 const cardTypes = ["TEST1", "TEST2", "TEST3"];
 
-beforeEach(() => {
-    when(axios.get)
-        .calledWith("http://localhost:8080/cards/types")
+describe("CardType component: ", () => {
+
+    beforeEach(() => {
+        when(axios.get)
+            .calledWith("http://localhost:8080/cards/types")
             .mockReturnValue(Promise.resolve({data: cardTypes}));
 
-});
+    });
 
-test('CardType should have one empty option', async () => {
-    await render(<CardType />)
+    it('Should have one empty option', async () => {
+        await render(<CardType/>)
 
-    const emptyOption = screen.getByRole("option", {name: ""});
-    expect(emptyOption).toBeInTheDocument();
-});
+        const emptyOption = screen.getByRole("option", {name: ""});
+        expect(emptyOption).toBeInTheDocument();
+    });
 
-test('CardType, empty option should be selected on load', () => {
-    render(<CardType />)
+    it('Empty option should be selected on load', () => {
+        render(<CardType/>)
 
-    const emptyOption = screen.getByRole("option", {name: "", selected: true});
-    expect(emptyOption).toBeInTheDocument();
-});
+        const emptyOption = screen.getByRole("option", {name: "", selected: true});
+        expect(emptyOption).toBeInTheDocument();
+    });
 
-test('CardType, empty option must be disabled', () => {
-    render(<CardType />)
+    it('Empty option must be disabled', () => {
+        render(<CardType/>)
 
-    const emptyOption = screen.getByRole("option", {name: "", selected: true});
-    expect(emptyOption).toBeDisabled();
-})
-
-test('CardType must call server to load all types', () => {
-    render(<CardType />);
-
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(axios.get).toHaveBeenCalledWith("http://localhost:8080/cards/types");
-});
-
-test('CardType should display all types get from server', async () => {
-    const wrapper = await shallow(<CardType/>);
-
-    cardTypes.forEach((currentType, index) => {
-        const generatedOption = wrapper.findWhere(node => node.key() === currentType);
-
-        expect(generatedOption.exists()).toBeTruthy();
-        expect(generatedOption.type()).toEqual("option");
-        expect(generatedOption.text()).toEqual(currentType);
+        const emptyOption = screen.getByRole("option", {name: "", selected: true});
+        expect(emptyOption).toBeDisabled();
     })
-});
 
-test('CardType should call function in parameter when value change', () => {
-    const triggerFunction = jest.fn();
-    const wrapper = shallow(<CardType handleChange={triggerFunction}/>);
-    expect(triggerFunction).not.toHaveBeenCalled();
+    it('Must call server to load all types', () => {
+        render(<CardType/>);
 
-    wrapper.find("select").simulate("change");
+        expect(axios.get).toHaveBeenCalledTimes(1);
+        expect(axios.get).toHaveBeenCalledWith("http://localhost:8080/cards/types");
+    });
 
-    expect(triggerFunction).toHaveBeenCalled();
-});
+    it('Should display all types get from server', async () => {
+        const wrapper = await shallow(<CardType/>);
 
-afterEach(() => {
-    jest.clearAllMocks();
+        cardTypes.forEach((currentType, index) => {
+            const generatedOption = wrapper.findWhere(node => node.key() === currentType);
+
+            expect(generatedOption.exists()).toBeTruthy();
+            expect(generatedOption.type()).toEqual("option");
+            expect(generatedOption.text()).toEqual(currentType);
+        })
+    });
+
+    it('Should call function in parameter when value change', () => {
+        const triggerFunction = jest.fn();
+        const wrapper = shallow(<CardType handleChange={triggerFunction}/>);
+        expect(triggerFunction).not.toHaveBeenCalled();
+
+        wrapper.find("select").simulate("change");
+
+        expect(triggerFunction).toHaveBeenCalled();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
 });
