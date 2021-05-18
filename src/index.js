4b7d7c6e9bc8b8e22980095718f45f4889c1344b
AlexDalencourt <alex.dalencourt@gmail.com>
2021-05-18 19:31:24 +0200

    add environment template

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import FileUpload from "./components/file_upload";
 import Boards from "./components/boards"
 
+const { REACT_APP_ENV } = process.env;
+console.log(REACT_APP_ENV);
+
 ReactDOM.render(
     <React.StrictMode>
         <FileUpload />
