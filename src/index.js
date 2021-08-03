f61b7178417670da76bdd63cae093af81ea0b97f
AlexDalencourt <alex.dalencourt@gmail.com>
2021-08-03 13:24:16 +0200

    toolbar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React, {Fragment, useContext, useState} from 'react';
+import React, {Fragment, useState} from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 
-import {AppBar, createMuiTheme, IconButton, ThemeProvider as MuiThemeProvider, Switch, Toolbar, Typography} from "@material-ui/core";
-import MenuIcon from '@material-ui/icons/Menu';
-import {getThemeByState} from './theme/theme';
+import clsx from "clsx";
+import {
+    createMuiTheme,
+    CssBaseline,
+    Divider,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    ThemeProvider as UiThemeProvider
+} from "@material-ui/core";
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import PublishIcon from '@material-ui/icons/Publish';
+import ViewComfyIcon from '@material-ui/icons/ViewComfy';
+
+import {getThemeByState, useStyles} from './theme/theme';
 
 import FileUpload from "./components/file_upload";
 import Boards from "./components/boards";
 import './style/index.css';
+import MunchkinToolbar from "./theme/toolbar";
+
 
 const { REACT_APP_ENV } = process.env;
 console.log(REACT_APP_ENV);
@@ -22,48 +39,89 @@ const ThemeProvider : React.FC = (props) => {
         _setThemeState(themeState);
     }
 
+    const classes = useStyles();
     return (
-        <ThemeContext.Provider value={{themeState, setThemeState}}>
-            <MuiThemeProvider theme={theme}>{props.children}</MuiThemeProvider>
+        <ThemeContext.Provider value={{themeState, setThemeState, classes}}>
+            <UiThemeProvider theme={theme}>{props.children}</UiThemeProvider>
         </ThemeContext.Provider>
     );
 }
 
-// const ThemeContext = React.createContext({themeState: false, setThemeName: createMuiTheme(getThemeByState(false))});
-const ThemeContext = React.createContext();
+export const ThemeContext = React.createContext();
 
 function App(){
-    const currentTheme = useContext(ThemeContext)
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
 
     return (
         <Fragment>
-            <AppBar position="static">
-                <Toolbar variant="dense">
-                    <IconButton edge="start"  color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" color="inherit">
-                        <img src={"Munchkin_banier.jpg"} alt={'Banier'} className={'banier'}>
-                        </img>
-                        <Switch checked={currentTheme.themeState === true} onClick={event => currentTheme.setThemeState(event.target.checked)}/>
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Router>
-                <Route exact path={'/'} component={Boards}/>
-                <Route path={'/upload'}>
-                    <FileUpload />
-                </Route>
-            </Router>
+            <div className={classes.root}>
+                <CssBaseline />
+                <MunchkinToolbar handleDrawerOpen={handleDrawerOpen} navigationState={open}/>
+                <Drawer
+                    variant="permanent"
+                    className={clsx(classes.drawer, {
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    })}
+                    classes={{
+                        paper: clsx({
+                            [classes.drawerOpen]: open,
+                            [classes.drawerClose]: !open,
+                        }),
+                    }}
+                >
+                    <div className={classes.toolbar}>
+                        <IconButton onClick={handleDrawerClose}>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List>
+                        <Link to={"/"}>
+                            <ListItem button key={"AllBoards"}>
+                                <ListItemIcon><ViewComfyIcon/></ListItemIcon>
+                                <ListItemText primary={"All boards"}/>
+                            </ListItem>
+                        </Link>
+                    </List>
+                    <Divider />
+                    <List>
+                        <Link to={"/upload"}>
+                            <ListItem button key={"Upload"}>
+                                <ListItemIcon><PublishIcon/></ListItemIcon>
+                                <ListItemText primary={"Upload new board"} />
+                            </ListItem>
+                        </Link>
+                    </List>
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar}/>
+                    <Route exact path={'/'} component={Boards}/>
+                    <Route path={'/upload'}>
+                        <FileUpload />
+                    </Route>
+                </main>
+            </div>
         </Fragment>
     )
-    }
-// }
+}
 
 ReactDOM.render(
     <React.StrictMode>
         <ThemeProvider>
-            <App />
+            <Router>
+                <App />
+            </Router>
         </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root')
